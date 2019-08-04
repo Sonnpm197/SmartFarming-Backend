7595a756309da7e1a2cbe22b924a4accc12ee931
sonn197 <sonnmp197@gmail.com>
2019-08-04 20:33:56 +0700

    12 article per page

--- src/main/java/com/son/CapstoneProject/common/ConstantValue.java
+++ src/main/java/com/son/CapstoneProject/common/ConstantValue.java
@@ -43,7 +43,7 @@ public class ConstantValue {
 
     // Pagination
     public static final int QUESTIONS_PER_PAGE = 3;
-    public static final int ARTICLES_PER_PAGE = 5;
+    public static final int ARTICLES_PER_PAGE = 12;
     public static final int TAGS_PER_PAGE = 5;
     public static final int USERS_PER_PAGE = 5;
     public static final int REPORTS_PER_PAGE = 5;
